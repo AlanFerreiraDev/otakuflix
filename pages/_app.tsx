11441f71eca005c4efe73d6cf967e57627cd4246
Alan Bruno Ferreira da Silva <alan.ferreira@pixter.com.br>
2023-05-22 20:28:09 -0300

    feat: add contexts and toast component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
+import { AuthProvider } from '@/context/authContext'
+import { AnimeProvider } from '@/context/animeContext'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthProvider>
+      <AnimeProvider>
+        <ToastContainer autoClose={3000} />
+        <Component {...pageProps} />
+      </AnimeProvider>
+    </AuthProvider>
+  )
 }
